df3d17b6317788ab1e93ef454ceca13d85573f3b
yangling <yl.tienon@gmail.com>
2015-05-17 21:55:14 +0800

    support prefer reader or perfer writer

--- liblwptw/pthread_rwlock_rdlock.c
+++ liblwptw/pthread_rwlock_rdlock.c
@@ -24,26 +24,32 @@ static const char rcsid[] =
 #include <errno.h>
 #include <windows.h>
 
+/*	读优先吗?*/
+#define	is_rwlock_prefer_reader(_rwlock_)		\
+	((_rwlock_)->_flags == 0)
+
+/*	没有写者 并且 (没有写排队或读优先)*/
 #define	try_hold_rdlock(_rwlock_)				\
 	(											\
+		((_rwlock_)->_writer == 0) &&			\
 		(										\
-			((_rwlock_)->_writer) |				\
-			((_rwlock_)->_nr_writers_queued)	\
-		) == 0									\
+			(!(_rwlock_)->_nr_writers_queued)||	\
+			is_rwlock_prefer_reader((_rwlock_))	\
+		)										\
 	)
 
 static int
-pthread_rwlock_rdlock_slow(pthread_rwlock_t * rwlock)
+pthread_rwlock_rdlock_slow(
+	pthread_rwlock_t * rwlock, int tid)
 {
 	int result = 0;
 	int _futex;
-	int tid = GetCurrentThreadId();
 
 	for(;;)
 	{
 		if(rwlock->_writer == tid)
 		{
-			/*	already held wrlock. deadlock*/
+			/*	holding WR by self. deadlock*/
 			result = EDEADLK;
 			break;
 		}
@@ -85,6 +91,7 @@ LIBLWPTW_API
 int pthread_rwlock_rdlock(pthread_rwlock_t * rwlock)
 {
 	int result = 0;
+	int tid = GetCurrentThreadId();
 
 	spin_acquire((spinlock_t*)&rwlock->_lock);
 
@@ -100,5 +107,5 @@ int pthread_rwlock_rdlock(pthread_rwlock_t * rwlock)
 		return result;
 	}
 
-	return pthread_rwlock_rdlock_slow(rwlock);
+	return pthread_rwlock_rdlock_slow(rwlock, tid);
 }
