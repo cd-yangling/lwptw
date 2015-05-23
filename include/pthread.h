4dce3f2e96ca983c3acb2d7d9c3f4d7ee868e003
yangling <yl.tienon@gmail.com>
2015-05-23 23:07:43 +0800

    add pthread_mutex_destroy.c

--- include/pthread.h
+++ include/pthread.h
@@ -124,6 +124,9 @@ int pthread_mutex_init(
 	pthread_mutex_t * mutex, const pthread_mutexattr_t *attr);
 
 LIBLWPTW_API
+int pthread_mutex_destroy(pthread_mutex_t * mutex);
+
+LIBLWPTW_API
 int pthread_mutex_lock(pthread_mutex_t * mutex);
 
 LIBLWPTW_API
---
+++ liblwptw/pthread_mutex_destroy.c
@@ -0,0 +1,27 @@
+/**
+ *	pthread_mutex_destroy.c
+ *
+ *	Copyright (C) 2014 YangLing(yl.tienon@gmail.com)
+ *
+ *	Description:
+ *
+ *	Revision History:
+ *
+ *	2015-05-15 Created By YangLing
+ *
+ *	implementation ²Î¿¼ glibc
+ */
+
+#ifndef lint
+static const char rcsid[] =
+	"@(#) $Id: pthread_mutex_destroy.c,"
+	"v 1.00 2015/05/15 09:00:00 CST yangling Exp $ (LBL)";
+#endif
+
+#include "pthread.h"
+
+LIBLWPTW_API
+int pthread_mutex_destroy(pthread_mutex_t * mutex)
+{
+	return 0;	/*	just let our compiler happy*/
+}
